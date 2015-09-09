7736c714085d7bfb920782b481eac20bd6a2d020
arobbins <arobbins@simpleblend.net>
2015-09-09 16:53:10 -0500

    Slight style changes

--- content/themes/simpleblend/templates/head.php
+++ content/themes/simpleblend/templates/head.php
@@ -14,7 +14,6 @@
   <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri() ?>/assets/imgs/apple-icon-144x144.png">
   <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri() ?>/assets/imgs/apple-icon-152x152.png">
   <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/assets/imgs/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri() ?>/assets/imgs/android-icon-192x192.png">
   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/assets/imgs/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri() ?>/assets/imgs/favicon-96x96.png">
   <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/assets/imgs/favicon-16x16.png">
